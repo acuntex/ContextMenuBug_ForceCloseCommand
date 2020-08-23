022ab2bacc104b1cbe359de25b097162f5a3753f
Werner Welsch <werner@welsch.cc>
2020-08-23 19:31:11 +0200

    initial commit

---
+++ ContextMenuBugForceCloseCommand/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using ContextMenuBugForceCloseCommand.ViewModel;
+using Xamarin.Forms;
+
+namespace ContextMenuBugForceCloseCommand
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<object>(this, "ShowDialog", async (obj) => await ShowDialog());
+            BindingContext = new MainPageViewModel();
+        }
+
+        private async Task ShowDialog()
+        {
+            await DisplayAlert("SideContextMenuView should close", "It should have been closed, but does this only on iOS.", "Cancel");
+        }
+    }
+}
---
+++ ContextMenuBugForceCloseCommand/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ContextMenuBugForceCloseCommand.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            TouchCommand = new Command(() => OnTouched());
+        }
+
+        public ICommand TouchCommand { get; set; }
+        public ICommand ForceCloseCommand { get; set; }
+        public bool AnimateCose { get; set; } = true;
+
+        private void OnTouched()
+        {
+            MessagingCenter.Send<object>(this, "ShowDialog");
+            if (AnimateCose)
+                ForceCloseCommand?.Execute(null);
+            else
+                ForceCloseCommand?.Execute(false);
+        }
+    }
+}
